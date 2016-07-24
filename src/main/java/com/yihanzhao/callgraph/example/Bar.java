e8732bef50264ab88f001baad8936d01c2cd7751
Yihan Zhao <yihanzhao@gmail.com>
2016-07-24 18:54:45 +1000

    Delete example package

--- src/main/java/com/yihanzhao/callgraph/example/Bar.java
+++
@@ -1,7 +0,0 @@
-package com.yihanzhao.callgraph.example;
-
-public class Bar {
-    public int getBar() {
-        return 1;
-    }
-}
--- src/main/java/com/yihanzhao/callgraph/example/Baz.java
+++
@@ -1,8 +0,0 @@
-package com.yihanzhao.callgraph.example;
-
-public class Baz {
-    public void show(Bar bar, Foo foo) {
-        int a = bar.getBar() + foo.getFoo(1);
-        System.out.println(a);
-    }
-}
